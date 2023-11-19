a9759c401bf831efb43a7e9ef70322aa3729aad6
Kenneth Massao Straumsheim <kmstraumsheim@gmail.com>
2023-11-19 21:50:31 +0100

    Optimalization 2 - (FPS 9.5)
    Reached an FPS of 9.5 with 1.7M points by lowering the amounts of calculations made.
    Before any further optimization, I am going to start on creating triangles for the point cloud.

--- Assets/Files/GroundConstructor.cs
+++ Assets/Files/GroundConstructor.cs
@@ -3,28 +3,25 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
-using Unity.VisualScripting;
-using UnityEditor;
-using UnityEditor.Build.Content;
 using UnityEngine;
 
 public class GroundConstructor : MonoBehaviour
 {
     public TextAsset cloudFile;
-    public GameObject point;
+    public GameObject pointPreFab;
     public GameObject pointBasket;
+    [SerializeField] Vector3 midPoint;
     [SerializeField] int pointAmount;
     private List<string> pointList;
     private List<Vector3> pointCloud;
-    private List<GameObject> pointObjects;
+    private List<Vector3> scaledCloud;
 
     // Start is called before the first frame update
     void Start()
     {
         pointList = new List<string>();
         pointCloud = new List<Vector3>();
-        pointObjects = new List<GameObject>();
+        scaledCloud = new List<Vector3>();
 
         // To transfer the file to the unity list
         ReadFile(cloudFile);
@@ -36,7 +33,7 @@ public class GroundConstructor : MonoBehaviour
         ListToPoints(pointList);
 
         // To create the physical pointCloud
-        PlacePoints(pointCloud);
+        //PlacePoints(scaledCloud);
 
         // Create triangles
     }
@@ -57,6 +54,11 @@ public class GroundConstructor : MonoBehaviour
 
         // Remove the first, count line
         pointList.Remove(pointList[0]);
+
+        midPoint = GetMidPoint(pointList);
+
+        for (int i = 0; i < pointList.Count(); i++)
+            scaledCloud.Add(StringToVector(pointList[i]) - midPoint);
     }
 
     void SetAmount(TextAsset file)
@@ -66,6 +68,7 @@ public class GroundConstructor : MonoBehaviour
         string[] allPoints = content.Split("\n");
         int count = allPoints.Count() - 1;
         int tryInt = 0;
+        StreamWriter stream;
 
         Debug.Log($"Den fyrste lina til fili segjer me hev {allPoints[0]} punkt");
         if (int.TryParse(allPoints[0], out tryInt))
@@ -73,7 +76,7 @@ public class GroundConstructor : MonoBehaviour
             Debug.Log("Fyrste lina er eit tal!");
             if (count != int.Parse(allPoints[0]))
             {
-                StreamWriter stream = new StreamWriter(Application.dataPath + "/files/32-1-490-163-56.txt", false);
+                stream = new StreamWriter(Application.dataPath + "/files/32-1-490-163-56.txt", false);
                 stream.Write((count).ToString() + "\n" + content);
                 stream.Close();
                 Debug.Log($"No hev me {count} punkt, og fili segjer me hev {allPoints[0]} punkt");
@@ -83,7 +86,7 @@ public class GroundConstructor : MonoBehaviour
         {
             Debug.Log("Fyrste lina er jo ikkje eit tal!");
 
-            StreamWriter stream = new StreamWriter(Application.dataPath + "/files/32-1-490-163-56.txt", false);
+            stream = new StreamWriter(Application.dataPath + "/files/32-1-490-163-56.txt", false);
             stream.Write((count + 1).ToString() + "\n" + content);
             stream.Close();
             Debug.Log($"No hev me {count + 1} punkt, og fili segjer me hev {allPoints[0]} punkt");
@@ -95,34 +98,23 @@ public class GroundConstructor : MonoBehaviour
     void ListToPoints(List<string> list)
     {
         for (int i = 0; i < list.Count(); i++)
-        {
             pointCloud.Add(StringToVector(list[i]));
-        }
+
+        //midPoint = new Vector3(pointCloud.Average(x => x.x), pointCloud.Average(x => x.y), pointCloud.Average(x => x.z));
     }
 
     void PlacePoints(List<Vector3> points)
     {
-        // Getting the average so that the points come closer to an orego
-        Vector3 average = new Vector3(points.Average(x => x.x), points.Average(x => x.y), points.Average(x => x.z));
-        Debug.Log($"Midpunktet er {average}");
-
         // Creation of the gameObject points
-        for (int i = 0; i < points.Count(); i++) 
-        {
+        for (int i = 0; i < points.Count(); i++)
             // Create GameObject at location
-            pointObjects.Add(Instantiate(point, points[i] - average, new Quaternion(0.0f, 0.0f, 0.0f, 0.0f)));
-            pointObjects[i].transform.SetParent(pointBasket.transform);
-        }
+            Instantiate(pointPreFab, points[i], new Quaternion(0.0f, 0.0f, 0.0f, 0.0f)).transform.SetParent(pointBasket.transform);
     }
-    
+
     Vector3 StringToVector(string str)
     {
         if (string.IsNullOrEmpty(str))
             return Vector3.zero;
-
-        float x = 0;
-        float y = 0;
-        float z = 0;
 
         string[] num = str.Split(' ');
         //Debug.Log($"string str {str} has been split into string[] num {num[0]}, {num[1]}, {num[2]}");
@@ -130,12 +122,29 @@ public class GroundConstructor : MonoBehaviour
         CultureInfo ci = (CultureInfo)CultureInfo.CurrentCulture.Clone();
         ci.NumberFormat.CurrencyDecimalSeparator = ".";
 
-        x = float.Parse(num[0], NumberStyles.Any, ci);
-        y = float.Parse(num[1], NumberStyles.Any, ci);
-        z = float.Parse(num[2], NumberStyles.Any, ci);
+        float x = float.Parse(num[0], NumberStyles.Any, ci);
+        float y = float.Parse(num[1], NumberStyles.Any, ci);
+        float z = float.Parse(num[2], NumberStyles.Any, ci);
         //Debug.Log($"string[] num {num[0]}, {num[1]}, {num[2]} has been Parsed into Vector3({x}, {y}, {z})");
 
         return new Vector3(x, z, y);
+    }
+
+    List<Vector3> StringToVector(List<string> strList)
+    {
+        List<Vector3> list = new List<Vector3>();
+
+        foreach (string str in strList)
+            list.Add(StringToVector(str));
+
+        return list;
+    }
+
+    Vector3 GetMidPoint(List<string> list)
+    {
+        Vector3 midPoint = new Vector3(StringToVector(list).Average(x => x.x), StringToVector(list).Average(x => x.y), StringToVector(list).Average(x => x.z));
+
+        return midPoint;
     }
 
     void CreateTriangles(List<Vector3> points)
@@ -151,9 +160,9 @@ public class GroundConstructor : MonoBehaviour
     {
         Gizmos.color = Color.white;
 
-        for (int i = 0; i < pointCloud.Count(); i++)
+        for (int i = 0; i < scaledCloud.Count(); i++)
         {
-            Gizmos.DrawLine(pointObjects[i].transform.position, pointObjects[i].transform.position + Vector3.up);
+            Gizmos.DrawLine(scaledCloud[i], scaledCloud[i] + Vector3.up);
         }
     }
 }
